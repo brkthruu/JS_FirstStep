361aad9ce0ecf584ecc671521dfe709c1ab2f659
hysimok <jeonghy95@gmail.com>
2020-07-06 00:55:43 +0900

    css fixed

--- nomad_vanillaJS/greeting.js
+++ nomad_vanillaJS/greeting.js
@@ -1,7 +1,7 @@
 /*
 ** get Username from user and save it in LocalStorage 
 */
-const	form = document.querySelector(".js-form"),
+const	form = document.querySelector(".js-nameform"),
 		input = form.querySelector("input"),
 		greeting = document.querySelector(".js-greetings");
 
